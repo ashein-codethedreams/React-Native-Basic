ee8016b618c299dc9e61f5a89af5e09e71f81fcd
ashein-codethedreams <heroglasses155@gmail.com>
2024-02-21 13:28:28 +0630

    adding small ui

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [isTitle, setIsTitle] = useState(false);
+  const handlePress = () => {
+    setIsTitle(!isTitle);
+  };
   return (
     <View style={styles.container}>
       <Text>Testing App V1</Text>
+      <Text>Hello</Text>
+      <Button title="Press Me" onPress={() => handlePress()}></Button>
+      <Text>{isTitle && "Pressed"}</Text>
     </View>
   );
 }
